caa317c7711542cbe96546d2c9f59864caefea67
lonzomagill <lonzomagill@gmail.com>
2023-01-15 21:12:09 +0800

    commit two sum solution

---
+++ com/fifmikey/TwoSum.java
@@ -0,0 +1,22 @@
+package com.fifmikey;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * leetcode two sum solution
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+}
