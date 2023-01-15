8c2ebadb99a7ef7d9e1be0d814ba84acd75de0af
lonzomagill <lonzomagill@gmail.com>
2023-01-15 21:06:50 +0800

    commit expproject class

---
+++ com/fifmikey/ExpProject.java
@@ -0,0 +1,15 @@
+package com.fifmikey;
+
+/**
+ * new project by fifmikey
+ * since 2023
+ */
+public class ExpProject {
+    private static final String s = "lalala";
+    private static final String m = "hihihihi";
+
+    public static void main(String[] args) {
+        String msg  = s + m;
+        System.out.println(msg);
+    }
+}
