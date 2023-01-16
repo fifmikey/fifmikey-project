728ab8e3599859ebe2b4f944351068ac90c5c857
lonzomagill <lonzomagill@gmail.com>
2023-01-16 21:24:21 +0800

    commit SubExample

---
+++ com/fifmikey/SumExample.java
@@ -0,0 +1,13 @@
+package com.fifmikey;
+
+public class SumExample {
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int a = 200;
+        int b = 100;
+        System.out.println(sub(a , b));
+    }
+}
